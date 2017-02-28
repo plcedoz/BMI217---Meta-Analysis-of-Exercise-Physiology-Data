273643e56bc856e497c4f7d5889b144ae23dfe16
Pierre-Louis CEDOZ <plcedoz@stanford.edu>
2017-02-27 23:45:47 -0800

    Compute differential gene list
    This scripts computes a list of differentially expressed genes using the limma package in R.

---
+++ compute_gene_list.R
@@ -0,0 +1,27 @@
+library(GEOquery)
+
+rm(list =ls())
+
+source("get_data_gse.R")
+
+gse_ref = "GSE8668"
+data_gse = get_data_gse(gse_ref)
+data_matrix = data_gse$data_matrix
+gsm_description = data_gse$gsm_description
+
+#We build the design matrix (case/control) from the gsm_description file
+after_exercise = gsm_description$V3 == "after-exercise"
+design = model.matrix(~ after_exercise)
+rownames(design) = gsm_description$V1
+fit <- lmFit (data_matrix, design)
+fit <- eBayes (fit)
+#Compute genes are differentially expressed at an adjusted p-value of ???? If we choose 0.05: 0 genes
+toptable <- toptable(fit, coef = 2,  p.value = 0.5, number = 50000)
+list_of_genes = toptable$ID
+
+#If we use Fold Change we don't have any genes!
+#toptable_lfc <- toptable(fit, coef = 2,  p.value = 0.5, lfc = 2^1.3, number = 50000)
+#number_lfc = dim(toptable_lfc)[1]
+
+
+
