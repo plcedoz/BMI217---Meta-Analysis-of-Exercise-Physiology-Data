d252d479b70c60527b2664b37150d4bf94f7b3b9
Pierre-Louis CEDOZ <plcedoz@stanford.edu>
2017-03-01 10:45:20 -0800

    Update compute_gene_list.R
    New file with the write.table function

--- compute_gene_list.R
+++ compute_gene_list.R
@@ -1,4 +1,6 @@
 library(GEOquery)
+library(limma)
+library(dplyr)
 
 rm(list =ls())
 
@@ -16,9 +18,10 @@ rownames(design) = gsm_description$V1
 fit <- lmFit (data_matrix, design)
 fit <- eBayes (fit)
 #Compute genes are differentially expressed at an adjusted p-value of ???? If we choose 0.05: 0 genes
-toptable <- toptable(fit, coef = 2,  p.value = 0.5, number = 50000)
-list_of_genes = toptable$ID
+toptable <- toptable(fit, coef = 2,  p.value = 1, number = 50000)
+list_of_genes = select(toptable, ID, P.Value, adj.P.Val)
 
+write.table(list_of_genes[1:1000,], sep = "\t", file = "gene_list_GSE8668", quote = FALSE, row.names = FALSE)
 #If we use Fold Change we don't have any genes!
 #toptable_lfc <- toptable(fit, coef = 2,  p.value = 0.5, lfc = 2^1.3, number = 50000)
 #number_lfc = dim(toptable_lfc)[1]
